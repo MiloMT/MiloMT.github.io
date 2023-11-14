78b8dead96771d9ee760e27e64c83613f668b2d8
MiloMT <mylestaylor496@gmail.com>
2023-11-15 08:55:14 +1000

    Added navigation easy-buttons

--- js/script.js
+++ js/script.js
@@ -2,155 +2,80 @@
 // No library version: https://jsfiddle.net/p1e9La6w/
 
 function Typer (element, options, scenes) {
-	// we define some default options to
-  // limit the need of adding extra configuration
 	var defaultOptions = { erase: 60, type: 80, break: 1000 };
   
-  // "config" is actually going
-  // to hold the options after they got merged
   var config = {};
   
-  // if options is an array, it means it's
-  // a list of scene and we didn't specify
-  // any options so in this case we're just
-  // swapping the arguments and setting an empty
-  // object for "options"
-  // this is great as it allows you to skip
-  // the options object if you're already ok
-  // with the defaults
   if (Array.isArray(options)) {
   	scenes = options;
     options = {};
   }
   
-  // here we loop over the defaultOptions properties
   for (var key in defaultOptions) {
-  	// that check ensures we're not looping over
-    // prototype properties, it's something that's
-    // related to how the for loop works but has nothing
-    // to do with this particular implementation ;)
   	if (!defaultOptions.hasOwnProperty(key)) continue;
     
     if (options[key] != null) {
-      // if there's an option provided for the
-      // given key, take it
     	config[key] = options[key];
     } else {
-    	// but otherwise, take it from the defaults
     	config[key] = defaultOptions[key];
     }
   }
   
-  // this status is what tells us
-  // whether the animation should be playing or not
   var status = 'ready';
-  
-  // state holds the currently typed value
-  // it's safer to rely on that internal state
-  // instead of the element's textContent as
-  // it could be changed by something else
-  // while this state variable cannot be changed
+
   var state = '';
-  
-  // currentSceneIndex store the index of
-  // the current playing scene
+
   var currentSceneIndex = 0
   
   function type () {
   	if (status !== 'playing') {
-    	// if the animation is not playing
-      // we don't want to do anything so just return
     	return;
     }
     
-    // let's get the current scene's value
   	var scene = scenes[currentSceneIndex];
     
-    // is the state the same as the scene?
-    // if so, it means we're done typing
     if (state === scene) {
-    	// so make a break and then erase the content
     	return setTimeout(erase, config.break);
     }
     
-    // if we made it until there,
-    // it means we still have some text to type
-    // we want to type the next character
-    // to do so we could get the current length
-    // and get the relevant character, e.g:
-    // scene.charAt(state.length + 1)
-    // but once again, the text content of the
-    // element could have been changed by
-    // something else so instead we're taking
-    // the scene's content from the start until
-    // the next character
     state = scene.substr(0, state.length + 1);
     element.textContent = state;
     
-    // after some delay, we're calling the same function
-    // (the one we are currently in)
-    // this way we'll type the next character if allowed/needed
-    // or just call the erase function
     setTimeout(type, config.type);
   }
   
   function erase () {
     if (status !== 'playing') {
-    	// same as type(), if the animation is not
-      // playing, there's nothing to do and we can just return
     	return;
     }
     
   	if (state === '') {
-    	// if the state is empty, it means
-      // we have erased everything and we
-      // want to play the next scene
-      // which is basically the currentScene + 1
     	currentSceneIndex++;
       
-      // but the current scene could have been the last
-      // if so, we want to loop over the scenario
-      // and replay it from scratch, setting the current scene to 0
       if (currentSceneIndex === scenes.length) {
       	currentSceneIndex = 0;
       }
       
-      // now that we have updated the current scene
-      // just ask to type() to type it
       return type();
     }
   
-  	// ok so if we're there, it means there are
-    // still some characters to erase
   	state = state.substr(0, state.length - 1);
     element.textContent = state;
     
-    // after some delay, erase the next character
     setTimeout(erase, config.erase);
   }
   
   function stop () {
-  	// as you've seen through erase and type
-    // the animation is stopped if the status
-    // is not "playing" so setting it to "ready"
-    // has the effect of stopping the animation
   	status = 'ready';
   }
   
   function play () {
   	if (status === 'ready') {
-    	// playing a scenario that is already
-      // playing is a non-sense so that's
-      // why we're ensuring that it's ready
-      // before playing it
     	status = 'playing';
     	type();
     }
   }
-  play();
   
-  // we're returning some functions that could be
-  // useful to users :)
   return { play: play, stop: stop };
 }
 
@@ -160,7 +85,186 @@ var player = Typer(roletyper, ['Full Stack Web Developer',
                             'Technology Enthusiast',
                             'Absolute Nerd']);
 
-// to specify some options, you could do:
-// var player = Typer(vader, { erase: 40, type: 60, break: 4000 }, ['Hello there!', 'How u doing?']);
-setTimeout(player.stop, 0)
-setTimeout(player.play, 1000)
+setTimeout(player.play, 1500)
+
+/* Easy Scroll Dots 2.0.3 --- https://github.com/Superhands89/EasyScrollDots
+* By Superhands89 2019-2022 --- https://github.com/Superhands89/
+* GPLv3 Licence for open source use only
+*/
+
+// a throttle function
+function dotsThrottle(func, wait, options) {
+  let context, args, result;
+  let timeout = null;
+  let previous = 0;
+  if (!options) options = {};
+  const later = function () {
+      previous = options.leading === false ? 0 : Date.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+  };
+  return function () {
+      const now = Date.now();
+      if (!previous && options.leading === false) previous = now;
+      const remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+          if (timeout) {
+              clearTimeout(timeout);
+              timeout = null;
+          }
+          previous = now;
+          result = func.apply(context, args);
+          if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+          timeout = setTimeout(later, remaining);
+      }
+      return result;
+  };
+};
+
+// global fixed nav and offset options
+let dotFixedNavPresent = false;
+let dotFixedNavId = '';
+let dotFixedNavUp = false;
+let dotOffset = 0;
+let dotNavHeight = 0;
+
+// scroll indicator controller
+function easyScrollDots(dotfixedOptions) {
+  let scrollIndi = document.querySelectorAll('[data-scroll-indicator-title]');
+  dotfixedOptions.fixedNav === true ? dotFixedNavPresent = true : dotFixedNavPresent;
+  dotfixedOptions.fixedNavId === '' ? dotFixedNavId = false : dotFixedNavId = dotfixedOptions.fixedNavId;
+  dotfixedOptions.fixedNavUpward === true ? dotFixedNavUp = true : dotFixedNavUp;
+  dotfixedOptions.offset > 0 ? dotOffset = dotfixedOptions.offset : dotOffset;
+
+  if (dotFixedNavPresent === true && dotFixedNavId.length) {
+  // there is a fixed nav and its id has been defined
+  const dotNavHeightElem = document.getElementById(dotFixedNavId);
+      if (dotNavHeightElem !== null) {
+    dotNavHeight = dotNavHeightElem.clientHeight;
+      }
+      else {
+    console.error('easyScrollDots Error: A fixed navigation ID has been defined, but an element with the corresponding ID cannot be found in the DOM. Check you have spelled the dotFixedNavId correctly.')
+      }
+  }
+
+  if (scrollIndi.length) {
+      const scrollIndiTemplate = '<div class="scroll-indicator-controller"><span></span></div>';
+      document.querySelector('body').lastElementChild.insertAdjacentHTML('afterend', scrollIndiTemplate);
+      const scrollIndiController = document.querySelector('.scroll-indicator-controller');
+      if ((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) { scrollIndiController.classList.add('indi-mobile'); }
+      const scrollIndiElems = Array.prototype.slice.call(scrollIndi);
+
+      scrollIndiElems.forEach(function (e, i) {
+          const scrollIndiTitle = e.getAttribute('data-scroll-indicator-title');
+          let firstActiveClass = '';
+
+          if (i == 0) {
+              firstActiveClass = 'active';
+          }
+          scrollIndiController.lastElementChild.insertAdjacentHTML('afterend', '<div class="' + firstActiveClass + '" data-indi-controller-id="' + scrollIndiTitle + '" onclick="scrollIndiClicked(\'' + scrollIndiTitle + '\');"><span>' + scrollIndiTitle + '</span><div></div></div>');
+          if (i == scrollIndiElems.length - 1) { // insert additional anchor so that we can always refer to previousElementSibling
+              scrollIndiController.lastElementChild.insertAdjacentHTML('afterend', '<div data-indi-controller-id="indiEnd" style="display: none;"></div>');
+              e.insertAdjacentHTML('afterend', '<div data-scroll-indicator-title="indiEnd"></div>');
+              scrollIndiElems.push(document.querySelector('[data-scroll-indicator-title="indiEnd"]'));
+          }
+      });
+
+      const scrollIndiControllerDots = scrollIndiController.querySelectorAll('[data-indi-controller-id]');
+
+      const handleIndiScroll = dotsThrottle(function () {
+          let indiScrollTopCollection = {};
+
+          scrollIndiElems.forEach(function (e) {
+              const scrollIndiIdScroll = e.getAttribute('data-scroll-indicator-title');
+              const indiScrollTop = e.getBoundingClientRect().top;
+
+              indiScrollTopCollection[scrollIndiIdScroll] = indiScrollTop;
+          });
+
+          // const indiOffsetValues = Object.values(indiScrollTopCollection); not supported in IE
+          const indiOffsetValues = Object.keys(indiScrollTopCollection).map(function (itm) { return indiScrollTopCollection[itm]; });
+    
+          const indiOffsetMin = function () {
+              const indiRemoveMinuses = indiOffsetValues.filter(function (x) { return x > 0 + (dotOffset + 2) + dotNavHeight; });
+
+              return Math.min.apply(null, indiRemoveMinuses);
+          };
+
+          Object.keys(indiScrollTopCollection).forEach(function (e) {
+
+              if (indiScrollTopCollection[e] == indiOffsetMin()) {
+
+                  Array.prototype.forEach.call(scrollIndiControllerDots, function (el) {
+                      if (el.classList.contains('active')) {
+                          el.classList.remove('active');
+                      }
+                  });
+
+                  const activeItemCandidate = scrollIndiController.querySelector('[data-indi-controller-id="' + e + '"]');
+                  const activeItemCandidatePrevEl = activeItemCandidate.previousElementSibling;
+                  const activeItem = activeItemCandidatePrevEl.tagName.toLowerCase() !== 'span' ? activeItemCandidatePrevEl : activeItemCandidate;
+
+                  activeItem.classList.add('active');
+              }
+          });
+
+      }, 300);
+
+      window.addEventListener('scroll', handleIndiScroll);
+  }
+};
+
+function scrollIndiClicked(indiId) {
+
+  const cachePageYOffset = window.pageYOffset;
+  const indiElement = document.querySelector('[data-scroll-indicator-title="' + indiId + '"]');
+
+  if (dotNavHeight !== 0) {
+
+      if (dotFixedNavUp === true) {
+          // fix nav on upward scroll only
+          window.scrollTo({
+      top: (cachePageYOffset + indiElement.getBoundingClientRect().top) - dotOffset,
+              left: 0,
+              behavior: 'smooth'
+          });
+          const scrollPos = document.body.getBoundingClientRect().top;
+          setTimeout(function () {
+              if (document.body.getBoundingClientRect().top > scrollPos) {
+                  window.scrollTo({
+        top: (cachePageYOffset + indiElement.getBoundingClientRect().top) - dotNavHeight - dotOffset,
+                      left: 0,
+                      behavior: 'smooth'
+                  });
+              }
+          }, 400);
+      }
+      else {
+          // fixed nav scroll
+          window.scrollTo({
+      top: (cachePageYOffset + indiElement.getBoundingClientRect().top) - dotNavHeight - dotOffset,
+              left: 0,
+              behavior: 'smooth'
+          });
+      }
+  }
+  else {
+      // normal scroll
+      window.scrollTo({
+    top: (cachePageYOffset + indiElement.getBoundingClientRect().top) - dotOffset,
+          left: 0,
+          behavior: 'smooth'
+      });
+  }
+};
+
+easyScrollDots({
+  'fixedNav': false,
+  'fixedNavId': '',
+  'fixedNavUpward': false,
+  'offset': 0
+});
